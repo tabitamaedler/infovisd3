5ac3b1baad41f101345ea36749e751defdcd6bcc
Paul Riedel <rpaul@hotmail.de>
2018-12-17 10:29:47 +0100

    Sunburst fertig

--- A_3/Paul/script/vis.js
+++ A_3/Paul/script/vis.js
@@ -1,271 +1,238 @@
-//linechart
-var margin = {top: 30, right: 10, bottom: 10, left: 10},
-    width = 1250 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+	rememberText=' ';
+        const width = window.innerWidth,
+            height = window.innerHeight,
+            maxRadius = (Math.min(width, height) / 2) - 5;
 
-var x = d3.scale.ordinal().rangePoints([0, width], 1),
-    y = {},
-	d = {},
-    dragging = {};
+        const formatNumber = d3.format(',d');
 
-var line = d3.svg.line(),
-    axis = d3.svg.axis().orient("left"),
-    background,
-    foreground;
+        const x = d3.scaleLinear()
+            .range([0, 2 * Math.PI])
+            .clamp(true);
 
-var svgLinechart = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-	.attr("class", "lineChart")
+        const y = d3.scaleSqrt()
+            .range([maxRadius*.1, maxRadius]);
+
+        const color = d3.scaleOrdinal(d3.schemeCategory20);
+
+        const partition = d3.partition();
+
+        const arc = d3.arc()
+            .startAngle(d => x(d.x0))
+            .endAngle(d => x(d.x1))
+            .innerRadius(d => Math.max(0, y(d.y0)))
+            .outerRadius(d => Math.max(0, y(d.y1)));
+
+        const middleArcLine = d => {
+            const halfPi = Math.PI/2;
+            const angles = [x(d.x0) - halfPi, x(d.x1) - halfPi];
+            const r = Math.max(0, (y(d.y0) + y(d.y1)) / 2);
+
+            const middleAngle = (angles[1] + angles[0]) / 2;
+            const invertDirection = middleAngle > 0 && middleAngle < Math.PI; // On lower quadrants write text ccw
+            if (invertDirection) { angles.reverse(); }
+
+            const path = d3.path();
+            path.arc(0, 0, r, angles[0], angles[1], invertDirection);
+            return path.toString();
+        };
+		  const outerArcLine = d => {
+            const halfPi = Math.PI/2;
+            const angles = [x(d.x0) - halfPi, x(d.x1) - halfPi];
+            const r = Math.max(0, (y(d.y0) + y(d.y1)) / 1.5);
+
+            const middleAngle = (angles[1] + angles[0]) / 2;
+            const invertDirection = middleAngle > 0 && middleAngle < Math.PI; // On lower quadrants write text ccw
+            if (invertDirection) { angles.reverse(); }
+
+            const path = d3.path();
+            path.arc(0, 0, r, angles[0], angles[1], invertDirection);
+            return path.toString();
+        };
+		 const innerArcLine = d => {
+            const halfPi = Math.PI/2;
+            const angles = [x(d.x0) - halfPi, x(d.x1) - halfPi];
+            const r = Math.max(0, (y(d.y0) + y(d.y1)) / 2.5);
+
+            const middleAngle = (angles[1] + angles[0]) / 2;
+            const invertDirection = middleAngle > 0 && middleAngle < Math.PI; // On lower quadrants write text ccw
+            if (invertDirection) { angles.reverse(); }
+
+            const path = d3.path();
+            path.arc(0, 0, r, angles[0], angles[1], invertDirection);
+            return path.toString();
+        };
+
+        const textFits = d => {
+            const CHAR_SPACE = 6;
+
+            const deltaAngle = x(d.x1) - x(d.x0);
+            const r = Math.max(0, (y(d.y0) + y(d.y1)) / 2);
+            const perimeter = r * deltaAngle;
+
+            return d.data.name.length * CHAR_SPACE < perimeter;
+        };
+
+        const svg = d3.select('body').append('svg')
+            .style('width', '100vw')
+            .style('height', '100vh')
+            .attr('viewBox', `${-width / 2} ${-height / 2} ${width} ${height}`)
+            .on('click', () => focusOn()); // Reset zoom on canvas click
+
+        d3.json('https://imld.de/docs/lehre/ws_18-19/data-vis/data/web-vis-tools.json', (error, root) => {
+            if (error) throw error;
+
+            root = d3.hierarchy(root);
+            root.sum(d => 1);
+
+            const slice = svg.selectAll('g.slice')
+                .data(partition(root).descendants());
+
+            slice.exit().remove();
+
+            const newSlice = slice.enter()
+                .append('g').attr('class', 'slice')
+				.attr('name', d => d.data.name)
+                .on('click', d => {
+                    d3.event.stopPropagation();
+                    focusOn(d);
+                })
+				.on('mouseover', d => {
+					infoOnDemand(d);
+				})
+		.on('mouseout', d=> {
+			hideInfo(d);
+		});
+
+            newSlice.append('title')
+                .text(d => d.data.name + '\n' + formatNumber(d.value));
+
+            newSlice.append('path')
+                .attr('class', 'main-arc')
+                .style('fill', d => color((d.children ? d : d.parent).data.name))
+                .attr('d', arc);
+
+            newSlice.append('path')
+                .attr('class', 'hidden-arc')
+                .attr('id', (_, i) => `hiddenArc${i}`)
+                .attr('d', middleArcLine);
+				
+				newSlice.append('path')
+                .attr('class', 'hidden-arc-outer')
+                .attr('id', (_, i) => `outerhiddenArc${i}`)
+                .attr('d', innerArcLine);
+				
+				newSlice.append('path')
+                .attr('class', 'hidden-arc-inner')
+               // .attr('id', (_, i) => `hiddenArc${i}`)
+                .attr('d', outerArcLine);
+
+            const text = newSlice.append('text')
+                .attr('display', d => textFits(d) ? null : 'none');
+
+            // Add white contour
+            text.append('textPath')
+                .attr('startOffset','50%')
+                .attr('xlink:href', (_, i) => `#hiddenArc${i}` )
+                .text(d => d.data.name)
+                .style('fill', 'none')
+                .style('stroke', '#fff')
+                .style('stroke-width', 5)
+                .style('stroke-linejoin', 'round');
+
+            text.append('textPath')
+                .attr('startOffset','50%')
+                .attr('xlink:href', (_, i) => `#hiddenArc${i}` )
+                .text(d => d.data.name);
+        });
+		/*
+		Ausgabe der Discription und des Values (alle Knoten des Astes/Bursts inc. den Ausgewaehlten/Root)
+		*/
+		function infoOnDemand(d){
+			i= svg.select('g[name="'+rememberText+'"]').select('.hidden-arc-outer').attr('id');
+				svg.select('g[name="'+rememberText+'"]')
+					.selectAll('textPath').attr('xlink:href', "#"+i  )
+               .text(d.data.description +'\n'+ d.value);
+		}
+		/* 
+		Die Zurücksetzen-funktion für den Nodenamen
+		*/
+		function hideInfo(d){
+			i= svg.select('g[name="'+rememberText+'"]').select('.hidden-arc').attr('id');
+				svg.select('g[name="'+rememberText+'"]')
+					.selectAll('textPath').attr('xlink:href', "#"+i  )
+               .text(' ');
+		}
+
+        function focusOn(d = { x0: 0, x1: 1, y0: 0, y1: 1 , data: 0}) {
+            // Reset to top-level if no data point specified
+		//	console.log(d);
 	
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-	
+            const transition = svg.transition()
+                .duration(750)
+                .tween('scale', () => {
+                    const xd = d3.interpolate(x.domain(), [d.x0, d.x1]),
+                        yd = d3.interpolate(y.domain(), [d.y0, 1]);
+                    return t => { x.domain(xd(t)); y.domain(yd(t)); };
+                });
 
-d3.csv("cars.csv", function(error, cars) {
-	
-  // Extract the list of dimensions and create a scale for each.
-  x.domain(dimensions = d3.keys(cars[0]).filter(function(d) {
-  switch(d){
-	case "Name":break;
-	case "Type":break;
-	//case "AWD":break;
-	//case "RWD":break;
-	default: return d && (y[d] = d3.scale.linear()
-        .domain(d3.extent(cars, function(p) { return +p[d]; }))
-        .range([height,0]));
-	};
-  }));
+            transition.selectAll('path.main-arc')
+                .attrTween('d', d => () => arc(d));
 
-  // Add grey background lines for context.
-  background = svgLinechart.append("g")
-      .attr("class", "background")
-    .selectAll("path")
-      .data(cars)
-    .enter().append("path")
-      .attr("d", path);
+            transition.selectAll('path.hidden-arc')
+                .attrTween('d', d => () => middleArcLine(d));
+			 transition.selectAll('path.hidden-arc-outer')
+                .attrTween('d', d => () => outerArcLine(d));
+				 transition.selectAll('path.hidden-arc-inner')
+                .attrTween('d', d => () => innerArcLine(d));
+            transition.selectAll('text')
+                .attrTween('display', d => () => textFits(d) ? null : 'none');
+            moveStackToFront(d);
 
-  // Add blue foreground lines for focus.
-  foreground = svgLinechart.append("g")
-      .attr("class", "foreground")
-    .selectAll("path")
-      .data(cars)
-    .enter().append("path")
-      .attr("d", path);
-
-  // Add a group element for each dimension.
-  var g = svgLinechart.selectAll(".dimension")
-      .data(dimensions)
-    .enter().append("g")
-      .attr("class", "dimension")
-      .attr("transform", function(d) { return "translate(" + x(d) + ")"; })
-      .call(d3.behavior.drag()
-        .origin(function(d) { return {x: x(d)}; })
-        .on("dragstart", function(d) {
-          dragging[d] = x(d);
-          background.attr("visibility", "hidden");
-        })
-        .on("drag", function(d) {
-          dragging[d] = Math.min(width, Math.max(0, d3.event.x));
-          foreground.attr("d", path);
-          dimensions.sort(function(a, b) { return position(a) - position(b); });
-          x.domain(dimensions);
-          g.attr("transform", function(d) { return "translate(" + position(d) + ")"; })
-        })
-        .on("dragend", function(d) {
-          delete dragging[d];
-          transition(d3.select(this)).attr("transform", "translate(" + x(d) + ")");
-          transition(foreground).attr("d", path);
-          background
-              .attr("d", path)
-            .transition()
-              .delay(500)
-              .duration(0)
-              .attr("visibility", null);
-        }));
-
-  // Add an axis and title.
-  g.append("g")
-      .attr("class", "axis")
-      .each(function(d) { d3.select(this).call(axis.scale(y[d])); })
-    .append("text")
-      .style("text-anchor", "middle")
-      .attr("y", -9)
-      .text(function(d) { return d; });
-
-  // Add and store a brush for each axis.
-  g.append("g")
-      .attr("class", "brush")
-      .each(function(d) {
-        d3.select(this).call(y[d].brush = d3.svg.brush().y(y[d]).on("brushstart", brushstart).on("brush", brush));
-      })
-    .selectAll("rect")
-      .attr("x", -8)
-      .attr("width", 16);
-});
-
-function position(d) {
-  var v = dragging[d];
-  return v == null ? x(d) : v;
-}
-
-function transition(g) {
-  return g.transition().duration(500);
-}
-
-// Returns the path for a given data point.
-function path(d) {
-  return line(dimensions.map(function(p) { return [position(p), y[p](d[p])]; }));
-}
-
-function brushstart() {
-  d3.event.sourceEvent.stopPropagation();
-}
-
-// Handles a brush event, toggling the display of foreground lines.
-function brush() {
-  var actives = dimensions.filter(function(p) { return !y[p].brush.empty(); }),
-      extents = actives.map(function(p) { return y[p].brush.extent(); });
-  foreground.style("display", function(d) {
-    return actives.every(function(p, i) {
-      return extents[i][0] <= d[p] && d[p] <= extents[i][1];
-    }) ? null : "none";
-  });
-}
-
-
-//Scatterplot 
-//http://bl.ocks.org/weiglemc/6185069
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
-/* 
- * value accessor - returns the value to encode for a given data object.
- * scale - maps value to a visual display encoding, such as a pixel position.
- * map function - maps from data value to display value
- * axis - sets up axis
- */ 
-
-// setup x 
-var xValue = function(d) { return d["Engine Size (l)"];}, // data -> value
-    xScale = d3.scale.linear().range([0, width]), // value -> display
-    xMap = function(d) { return xScale(xValue(d));}, // data -> display
-    xAxis = d3.svg.axis().scale(xScale).orient("bottom");
-
-	
-// setup y
-var yValue = function(d) {  return d["Horsepower(HP)"];}, // data -> value
-    yScale = d3.scale.linear().range([height, 0]), // value -> display
-    yMap = function(d) { return yScale(yValue(d));}, // data -> display
-    yAxis = d3.svg.axis().scale(yScale).orient("left");
-	
-	var zValue =function(d) {  return d.Cyl;},
-		size = d3.scale.category10();
+		/*
+			zurücksetzten der Ausgabe Position für den Namen
 		
-	var wValue= function(d){return d.Len;},
-		  wScale = d3.scale.linear().range([0, 10]);
 		
-	  console.log(wValue.domain());
-
-// add the graph canvas to the body of the webpage
-var svgPlotter = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-	.attr("class", "Plotter")	
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-// add the tooltip area to the webpage
-var tooltip = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
-
-// load data
-d3.csv("cars.csv", function(error, data) {
-
-  // change string (from CSV) into number format
-  data.forEach(function(d) {
-    d.size = +d.size;
-    d["Engine Size(1)"] = +d["Engine Size (l)"];
-  //  console.log(y["Horsepower(HP)"]);
-  });
-  
-	  console.log(size.domain());
-
-  // don't want dots overlapping axis, so add in buffer to data domain
-  xScale.domain([d3.min(data, xValue), d3.max(data, xValue)]);
-  yScale.domain([d3.min(data, yValue)-100, d3.max(data, yValue)+1]);
-  wScale.domain([d3.min(data, wValue), d3.max(data, wValue)]);
-  
-	  console.log(wScale.domain());
-  // x-axis
-  svgPlotter.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-    .append("text")
-      .attr("class", "label")
-      .attr("x", width)
-      .attr("y", -6)
-      .style("text-anchor", "end")
-      .text("Engine Size(1)");
-
-  // y-axis
-  svgPlotter.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("class", "label")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("");
-
+		*/
+		hideInfo(d);
+		/*
+			-merken des aktuellen Mittelknotens 
+			-Änderung des Labels im Textbereich
+		*/
+			if( d.data != 0){
+				
+				if(d.depth = 0){
+					
+					svg.select('g[name="'+rememberText+'"]')
+					.selectAll('textPath').text(rememberText);
+				}
+					else
+					{
+						if(rememberText != ''){
+						svg.select('g[name="'+rememberText+'"]')
+						.selectAll('textPath').text(rememberText);
+						}
+						
+					
+					rememberText=svg.select('g[name="'+d.data.name+'"]')
+					.selectAll('textPath').text();
+					svg.select('g[name="'+d.data.name+'"]')
+					.selectAll('textPath').text('');
+					}
+				}
+				else{
 	
-	  
-  // draw dots
-  svgPlotter.selectAll(".dot")
-      .data(data)
-    .enter().append("circle")
-      .attr("class", "dot")
-      .attr("r", function(d){ 	return wScale(wValue(d))})
-      .attr("cx", xMap)
-      .attr("cy", yMap)
-      .style("fill", function(d) { return size(zValue(d));}) 
-      .on("mouseover", function(d) {
-          tooltip.transition()
-               .duration(200)
-               .style("opacity", .9);
-          tooltip.html(d["Name"] + "<br/> (" + xValue(d) 
-	        + ", " + yValue(d) + ", "+ zValue(d) + ")")
-               .style("left", (d3.event.pageX + 5) + "px")
-               .style("top", (d3.event.pageY - 28) + "px");
-      })
-      .on("mouseout", function(d) {
-          tooltip.transition()
-               .duration(500)
-               .style("opacity", 0);
-      });
-
-  // draw legend
-  //sortierung der Legende
-  	  var sorted = size.domain().sort(function(a, b) {
-	  return d3.ascending(a, b);});
-  var legend = svgPlotter.selectAll(".legend")
-      .data(sorted)
-    .enter().append("g")
-      .attr("class", "legend")
-      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-
-  // draw legend colored rectangles
-  legend.append("rect")
-      .attr("x", width - 18)
-      .attr("width", 18)
-      .attr("height", 18)
-      .style("fill", size);
-
-
-
-      
+					svg.select('g[name="'+rememberText+'"]')
+					.selectAll('textPath').text(rememberText);
+				
+				
+				}
+				
+            function moveStackToFront(elD) {
+                svg.selectAll('.slice').filter(d => d === elD)
+                    .each(function(d) {
+                        this.parentNode.appendChild(this);
+                        if (d.parent) { moveStackToFront(d.parent); }
+                    })
+            }
+        }
+    
